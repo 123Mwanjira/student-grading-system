2d2f643165a36a01fb8caaf27bc8e731e400910a
Maurine <maureen.wanjira@gmail.com>
2026-03-29 18:06:40 +0300

    First grading system project

---
+++ grading.js
@@ -0,0 +1,11 @@
+let marks = 75;
+
+if (marks >= 80) {
+  console.log("A - Excellent");
+} else if (marks >= 60) {
+  console.log("B - Good");
+} else if (marks >= 50) {
+  console.log("C - Average");
+} else {
+  console.log("F - Fail");
+}
